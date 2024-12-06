50480bf9211bbc98e3bd762335535ade0a7436ce
Vitaly1123 <nezhura.vitalii1123@vu.cdu.edu.ua>
2024-12-07 01:20:27 +0200

    Написав прості оператори та трішки MyFracData

---
+++ MyFraction/FractionOperation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFraction
+{
+    partial class MyFrac
+    {
+        public double ToDouble()
+        {
+            return (double)this.nom / this.denom;
+        }
+
+        public static MyFrac operator +(MyFrac f1, MyFrac f2)
+        {
+            return new MyFrac(f1.nom * f2.denom + f1.denom * f2.nom, f1.denom * f2.denom);
+        }
+
+        public static MyFrac operator -(MyFrac f1, MyFrac f2)
+        {
+            return new MyFrac(f1.nom * f2.denom - f1.denom * f2.nom, f1.denom * f2.denom);
+        }
+
+        public static MyFrac operator *(MyFrac f1, MyFrac f2)
+        {
+            return new MyFrac(f1.nom * f2.nom, f1.denom * f2.denom);
+        }
+
+        public static MyFrac operator /(MyFrac f1, MyFrac f2)
+        {
+            return new MyFrac(f1.nom * f2.denom, f1.denom * f2.nom);
+        }
+    }
+}
