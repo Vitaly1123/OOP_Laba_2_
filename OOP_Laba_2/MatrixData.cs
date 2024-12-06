3d516dfa92595fe4ff40870137b21555fe4b1ee6
Vitaly1123 <nezhura.vitalii1123@vu.cdu.edu.ua>
2024-12-06 23:26:54 +0200

    Додав оператори множенення і додавання та конструктори.

--- OOP_Laba_2/MatrixData.cs
+++ OOP_Laba_2/MatrixData.cs
@@ -5,5 +5,49 @@ namespace Matrix
 {
     public partial class MatrixData
     {
+        protected double[,] matrix;
+        protected double? cachedDet = null;
+        protected bool isMod = true;
+        protected void InvalidateCache()
+        {
+            isMod = true;
+            cachedDet = null;
+        }
+        public int Height //row
+        {
+            get => matrix.GetLength(0);
+        }
+        public int Width // col
+        {
+            get => matrix.GetLength(1);
+        }
+        public MyMatrix(MyMatrix other)
+        {
+            this.matrix = (double[,])other.matrix.Clone();
+        }
+        public MyMatrix(double[,] multiDimensionsMatrix)
+        {
+            this.matrix = (double[,])multiDimensionsMatrix.Clone();
+        }
+        public MyMatrix(double[][] jaggedMatrix)
+        {
+            int height = jaggedMatrix.Length;
+            int width = jaggedMatrix[0].Length;
+            for (int i = 0; i < height; i++)
+            {
+                if (jaggedMatrix[i].Length != width)
+                {
+                    throw new ArgumentException("Усі підмасиви повинні мати однакову кількість елементів.");
+                }
+            }
+            matrix = new double[height, width];
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    matrix[i, j] = jaggedMatrix[i][j];
+                }
+            }
+        }
     }
 }
---
+++ OOP_Laba_2/MatrixOperation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    public partial class MyMatrix
+    {
+        public static MyMatrix operator +(MyMatrix a, MyMatrix b)
+        {
+
+            if (a.Height != b.Height || a.Width != b.Width)
+            {
+                throw new ArgumentException("Матриця повинна бути однакового розміру");
+            }
+
+            MyMatrix result = new MyMatrix(a);
+
+            for (int i = 0; i < a.Height; i++)
+            {
+                for (int j = 0; j < b.Height; j++)
+                {
+                    result[i, j] = a[i, j] + b[i, j];
+                }
+            }
+
+            return result;
+        }
+
+        public static MyMatrix operator *(MyMatrix a, MyMatrix b)
+        {
+            if (a.Height != b.Height || a.Width != b.Width)
+            {
+                throw new ArgumentException("Матриця повинна бути однакового розміру");
+            }
+            MyMatrix result = new MyMatrix(new double[a.Height, b.Width]);
+
+            for (int i = 0; i < a.Height; i++)
+            {
+                for (int j = 0; j < b.Width; j++)
+                {
+                    double sum = 0;
+                    for (int k = 0; k < a.Width; k++)
+                    {
+                        sum += a[i, k] * b[k, j];
+                    }
+                    result[i, j] = sum;
+                }
+            }
+
+            return result;
+        }
+    }
+}
